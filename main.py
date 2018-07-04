ed5ec5acd40e4a65d2aba4f42664dccd99e0502e
Jose Machado <2152133@my.ipleiria.pt>
2018-07-04 13:21:05 +0100

    Implements continue_crawl(search_history, target_url) function

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+def continue_crawl(search_history, target_url):
+    if search_history[-1] == target_url:
+        return False
+    if len(search_history) > 25:
+        return False
+    i = 0
+    while(i < len(search_history)):
+        j = i+1
+        while(j < len(search_history)):
+            if search_history[i] == search_history[j]:
+                return False
+            j += 1
+        i += 1
+    return True
 
